45dbed7c42d6ee20271351a2264cad87dbac5a8c
minjae46 <minjaejo46@gmail.com>
2023-02-14 19:37:58 +0900

    Add: logger

--- src/server.js
+++ src/server.js
@@ -1,10 +1,23 @@
 import express from "express";
-
-const app = express();
+import morgan from "morgan";
 
 const PORT = 4000;
 
+const app = express();
+const logger = morgan("dev");
+
+const home = (req, res) => {
+  return res.send("Hello");
+};
+const login = (req, res) => {
+  return res.send("login");
+};
+
+app.use(logger);
+app.get("/", home);
+app.get("/login", login);
+
 const handleListening = () =>
-  console.log(`Server listening on port http://localhost:${PORT}`);
+  console.log(`Server listening on port http://localhost:${PORT}.`);
 
 app.listen(PORT, handleListening);
