abf7d891216888cc1dc611449d55c3a2c13dac1e
minjae46 <minjaejo46@gmail.com>
2023-10-05 16:55:03 +0900

    Refactor Join

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -17,18 +17,28 @@ export const postJoin = async (req, res) => {
       errorMessage: "This username/email is already taken.",
     });
   }
-  await User.create({
-    name,
-    username,
-    email,
-    password,
-    location,
-  });
-  return res.redirect("/login");
+  try {
+    await User.create({
+      name,
+      username,
+      email,
+      password,
+      location,
+    });
+    return res.redirect("/login");
+  } catch (error) {
+    return res.status(400).render("join", {
+      pageTitle: "Upload Video",
+      errorMessage: error._message,
+    });
+  }
 };
 
 export const edit = (req, res) => res.send("Edit User");
 export const remove = (req, res) => res.send("Delete User");
+
 export const login = (req, res) => res.send("Login");
+
 export const logout = (req, res) => res.send("Logout");
+
 export const see = (req, res) => res.send("See User");
