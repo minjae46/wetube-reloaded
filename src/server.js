9225649d06279335fcfaed092847100bff1a3a45
minjae46 <minjaejo46@gmail.com>
2024-05-09 00:04:23 +0900

    주석 수정

--- src/server.js
+++ src/server.js
@@ -11,7 +11,7 @@ app.set("view engine", "pug");
 app.set("views", process.cwd() + "/src/views");
 app.use(logger);
 app.use(express.urlencoded({ extended: true }));
-//form의 value를 자바스크립트 object 형식으로 변환해주어 express app이 form 데이터를 다룰 수 있게 해주는 메서드.
+//form의 value를 자바스크립트 object 형식으로 변환하여 express app이 form 데이터를 다룰 수 있게 해주는 메서드.
 
 app.use("/", routeRouter);
 app.use("/users", userRouter);
