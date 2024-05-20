34885d66ac756bffa98ef55342e3527c0d9d5b83
minjae46 <minjaejo46@gmail.com>
2024-05-20 22:47:10 +0900

    WIP

--- src/init.js
+++ src/init.js
@@ -9,3 +9,6 @@ const handleListening = () =>
   console.log(`✅ Server listening on port http://localhost:${PORT}.`);
 
 app.listen(PORT, handleListening);
+
+// init.js 는 서버 실행 전에 필요한 모든 것들을 import 시키는 역할을 담당.
+// db 등 필요한 파일들을 모두 import한 후에 서버 실행.
